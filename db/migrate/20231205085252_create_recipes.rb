a859ed1416a571d99a7bfb44803935670a4f0bed
binyamyohannes <olangobinyam@gmail.com>
2023-12-05 13:20:18 +0300

    Migrate create user and recipe

--- db/migrate/20231205085252_create_recipes.rb
+++ db/migrate/20231205085252_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[7.1]
   def change
     create_table :recipes do |t|
-      t.string :name
+      t.string :name, null: false, default: ""
       t.text :description
       t.decimal :preparation_time, precision: 5, scale: 2
       t.decimal :cooking_time, precision: 5, scale: 2
@@ -9,5 +9,8 @@ class CreateRecipes < ActiveRecord::Migration[7.1]
 
       t.timestamps
     end
+
+    add_index :recipes, :name, unique: true
+    add_reference :recipes, :user, null: false, foreign_key: { on_delete: :cascade }
   end
 end
