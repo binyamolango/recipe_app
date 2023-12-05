b55c7146cb093e3062a1fab3b5b9ff7d92dbc1a8
binyamyohannes <olangobinyam@gmail.com>
2023-12-05 12:36:13 +0300

    Update migration

--- db/migrate/20231205085252_create_recipes.rb
+++ db/migrate/20231205085252_create_recipes.rb
@@ -3,9 +3,9 @@ class CreateRecipes < ActiveRecord::Migration[7.1]
     create_table :recipes do |t|
       t.string :name
       t.text :description
-      t.string :preparation_time
-      t.string :cooking_time
-      t.boolean :public
+      t.decimal :preparation_time, precision: 5, scale: 2
+      t.decimal :cooking_time, precision: 5, scale: 2
+      t.boolean :public, default: true
 
       t.timestamps
     end
