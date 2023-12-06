4f5292c381a763702c52d19b2471e3a1a47da219
binyamyohannes <olangobinyam@gmail.com>
2023-12-06 12:39:11 +0300

    Update add new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -65,6 +65,6 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:title, :content)
+      params.require(:recipe).permit(:name, :description)
     end
 end
