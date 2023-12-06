0761b8b4708be6281a59dc09809a50fdb7e03645
binyamyohannes <olangobinyam@gmail.com>
2023-12-06 15:10:06 +0300

    Update recipe_params in recipes controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { maximum: 250 }
-  validates :preparation_time, numericality: { allow_nil: true, greater_than_or_equal_to: 0 }
-  validates :cooking_time, numericality: { allow_nil: true, greater_than_or_equal_to: 0 }
+  validates :preparation_time, numericality: { greater_than_or_equal_to: 0 }
+  validates :cooking_time, numericality: { greater_than_or_equal_to: 0 }
 end
