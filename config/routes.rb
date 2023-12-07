4c055c5d162201930aaf1c07b6cf673bfc52f9b9
binyamyohannes <olangobinyam@gmail.com>
2023-12-07 15:05:10 +0300

    Generate food and recipe_food

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :recipe_foods
+  resources :foods
   devise_for :users, :controllers => { :confirmations => 'devise/confirmations' }
 
   devise_scope :user do
