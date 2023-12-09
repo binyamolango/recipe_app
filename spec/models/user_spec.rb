ea037455bc04bfc87524213ed493a6e35af87fef
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2023-12-09 22:35:03 +0500

    unit tests added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { is_expected.to have_many(:foods).with_foreign_key(:user_id).dependent(:destroy) }
+    it { is_expected.to have_many(:recipes).with_foreign_key(:user_id).dependent(:destroy) }
+    it { is_expected.to have_many(:inventories).with_foreign_key(:user_id).dependent(:destroy) }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+  end
 end
